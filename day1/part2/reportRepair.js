1b9b4fcdb5bd69e0ac8ab581898cff641232dc57
wusung <ussvpn941@gmail.com>
2020-12-02 10:19:25 +0800

    add day 1 part 2

--- day1/part2/reportRepair.js
+++ day1/part2/reportRepair.js
@@ -4,29 +4,34 @@ import * as path from "https://deno.land/std@0.79.0/path/mod.ts";
 const filename = path.join(Deno.cwd(), "./input.txt");
 let fileReader = await Deno.open(filename);
 
+
+// prepare data
 let arr1 = []
 let arr2 = []
+let arr3 = []
 for await (let line of readLines(fileReader)) {
 //   console.log(line);
   if (line !== '') {
     arr1.push(parseInt(line))
     arr2.push(parseInt(line))
+    arr3.push(parseInt(line))
   }
 }
 
 let expectedSum = 2020;
-let expectedMul = 0;
 
 let results = []
 for (let i of arr1) {
     for (let j of arr2) {
-        // console.log(`${i} + ${j} = ${i + j}`)
-        if ((i + j) == expectedSum) {
-            results.push([i, j])
+        for (let k of arr3) {
+          if ((i + j + k) == expectedSum) {
+              results.push([i, j, k])
+          }
         }
     }
 }
 console.log(results)
+
 for (let i of results) {
-  console.log(i[0] * i[1])
+  console.log(i[0] * i[1] * i[2])
 }
